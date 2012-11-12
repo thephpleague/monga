fd90d9ef3455c06bc23ceac97a2245a11c7d874e
Frank de Jonge <info@frenky.net>
2012-11-12 20:34:09 +0100

    Added Query\Remove tests

---
+++ tests/QueryRemoveTests.php
@@ -0,0 +1,47 @@
+<?php
+
+
+class QueryBuilderTests extends PHPUnit_Framework_TestCase
+{
+	protected $remove;
+
+	public function setUp()
+	{
+		$this->remove = new Monga\Query\Remove();
+	}
+
+	public function getProperty($property)
+	{
+		$reflection = new ReflectionObject($this->remove);
+		$property = $property = $reflection->getProperty($property);
+		$property->setAccessible(true);
+		return $property->getValue($this->remove);
+	}
+
+	public function testSingle()
+	{
+		$this->remove->single();
+		$this->assertTrue($this->getProperty('justOne'));
+		$this->remove->single(false);
+		$this->assertFalse($this->getProperty('justOne'));
+	}
+
+	public function testMultiple()
+	{
+		$this->remove->multiple();
+		$this->assertFalse($this->getProperty('justOne'));
+		$this->remove->multiple(false);
+		$this->assertTrue($this->getProperty('justOne'));
+	}
+
+	public function testGetOptions()
+	{
+		$this->remove->single();
+		$this->assertEquals(array(
+			'safe' => false,
+			'fsync' => false,
+			'timeout' => MongoCursor::$timeout,
+			'justOne' => true
+		), $this->remove->getOptions());
+	}
+}
