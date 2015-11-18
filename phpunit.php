8c4bc5cb42ef8c32a489a00cbca2af3ebdc1bfac
Bryan Crowe <bryan@bryan-crowe.com>
2015-11-18 17:56:58 -0500

    Remove old mockery bug fix

--- phpunit.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-include './vendor/autoload.php';
-
-// Remove when fix is merged.
-Mockery::getConfiguration()->setInternalClassMethodParamMap('MongoCollection', "aggregate", ['$pipeline', '$op = NULL', '$third = NULL']);
